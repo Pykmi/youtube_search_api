5a64a4aaadf407f3464e3bde2322418810c1112a
Tomi Kaistila <tomi.kaistila@gofore.com>
2020-03-03 11:05:11 +0200

    Last minute tweaks, added mocked data.

---
+++ mockApiCall.js
@@ -0,0 +1,185 @@
+function mockApiCall() {
+  return {
+    kind: "youtube#searchListResponse",
+    etag: '"SJZWTG6xR0eGuCOh2bX6w3s4F94/nlIrEVWKUOsJSzWah7rW_nnPfyk"',
+    nextPageToken: "CAUQAA",
+    regionCode: "FI",
+    pageInfo: {
+      totalResults: 1000000,
+      resultsPerPage: 5
+    },
+    items: [
+      {
+        kind: "youtube#searchResult",
+        etag: '"SJZWTG6xR0eGuCOh2bX6w3s4F94/n5zkS73ly2jkR5NPCbTZQbI5LXA"',
+        id: {
+          kind: "youtube#video",
+          videoId: "SB-qEYVdvXA"
+        },
+        snippet: {
+          publishedAt: "2018-03-19T02:00:25.000Z",
+          channelId: "UCq5hgY37WAryZCwmehDyCaQ",
+          title: "So many cute kittens videos compilation 2018",
+          description:
+            "Bravecto #anipetshop.com ✈✈✈Shop Pet Medicines : https://anipetshop.com ©BUY NOW : Bravecto, Nexgard, Heartgard, Revolution,..... ▻Worldwide FREE ...",
+          thumbnails: {
+            default: {
+              url: "https://i.ytimg.com/vi/SB-qEYVdvXA/default.jpg",
+              width: 120,
+              height: 90
+            },
+            medium: {
+              url: "https://i.ytimg.com/vi/SB-qEYVdvXA/mqdefault.jpg",
+              width: 320,
+              height: 180
+            },
+            high: {
+              url: "https://i.ytimg.com/vi/SB-qEYVdvXA/hqdefault.jpg",
+              width: 480,
+              height: 360
+            }
+          },
+          channelTitle: "Funny Videos",
+          liveBroadcastContent: "none"
+        }
+      },
+      {
+        kind: "youtube#searchResult",
+        etag: '"SJZWTG6xR0eGuCOh2bX6w3s4F94/1y3ELFtYNcEdFzSHEvmNU5kVthc"',
+        id: {
+          kind: "youtube#video",
+          videoId: "Uxv1_lZoYew"
+        },
+        snippet: {
+          publishedAt: "2020-01-25T12:00:04.000Z",
+          channelId: "UC5VvB6tmC4lu_WCx7hE0PZA",
+          title: "SO RELAXING: Watch kittens grow up in 10 minutes!",
+          description:
+            "Foster kittens grow up so fast! In this peaceful POV video, you can watch Gourdon and Pepita transform from newborn kittens to 8 week old adoptable kittens.",
+          thumbnails: {
+            default: {
+              url: "https://i.ytimg.com/vi/Uxv1_lZoYew/default.jpg",
+              width: 120,
+              height: 90
+            },
+            medium: {
+              url: "https://i.ytimg.com/vi/Uxv1_lZoYew/mqdefault.jpg",
+              width: 320,
+              height: 180
+            },
+            high: {
+              url: "https://i.ytimg.com/vi/Uxv1_lZoYew/hqdefault.jpg",
+              width: 480,
+              height: 360
+            }
+          },
+          channelTitle: "Kitten Lady",
+          liveBroadcastContent: "none"
+        }
+      },
+      {
+        kind: "youtube#searchResult",
+        etag: '"SJZWTG6xR0eGuCOh2bX6w3s4F94/2700VcVB7e30vOTJE9I2hBWjaJI"',
+        id: {
+          kind: "youtube#video",
+          videoId: "5_nWGG_TFDM"
+        },
+        snippet: {
+          publishedAt: "2019-08-22T15:00:13.000Z",
+          channelId: "UC8P4HIRzjjnxU-MQX0QHLUw",
+          title: "A very patient mother cat with 5 crazy active kittens !!",
+          description:
+            "These 5 extremely hyperactive kittens are playing and running like crazy and the mother cat is being very patient with them. The kittens are fighting each other in ...",
+          thumbnails: {
+            default: {
+              url: "https://i.ytimg.com/vi/5_nWGG_TFDM/default.jpg",
+              width: 120,
+              height: 90
+            },
+            medium: {
+              url: "https://i.ytimg.com/vi/5_nWGG_TFDM/mqdefault.jpg",
+              width: 320,
+              height: 180
+            },
+            high: {
+              url: "https://i.ytimg.com/vi/5_nWGG_TFDM/hqdefault.jpg",
+              width: 480,
+              height: 360
+            }
+          },
+          channelTitle: "walter santi",
+          liveBroadcastContent: "none"
+        }
+      },
+      {
+        kind: "youtube#searchResult",
+        etag: '"SJZWTG6xR0eGuCOh2bX6w3s4F94/mGqWSvZXNjRkO8jJFr4Q7trKmrc"',
+        id: {
+          kind: "youtube#video",
+          videoId: "6oeVTnrsBmo"
+        },
+        snippet: {
+          publishedAt: "2020-02-28T10:00:01.000Z",
+          channelId: "UC5ggtACLoJ3Ro8RNNgKfcvA",
+          title: "😍 Supper Cute Kittens In The World #5 - Cute VN",
+          description:
+            "Thank you for watching, I hope you enjoyed! ▷ Watch more cute animal compilations! https://youtube.com/playlist?list=UU5ggtACLoJ3Ro8RNNgKfcvA ...",
+          thumbnails: {
+            default: {
+              url: "https://i.ytimg.com/vi/6oeVTnrsBmo/default.jpg",
+              width: 120,
+              height: 90
+            },
+            medium: {
+              url: "https://i.ytimg.com/vi/6oeVTnrsBmo/mqdefault.jpg",
+              width: 320,
+              height: 180
+            },
+            high: {
+              url: "https://i.ytimg.com/vi/6oeVTnrsBmo/hqdefault.jpg",
+              width: 480,
+              height: 360
+            }
+          },
+          channelTitle: "Cute VN",
+          liveBroadcastContent: "none"
+        }
+      },
+      {
+        kind: "youtube#searchResult",
+        etag: '"SJZWTG6xR0eGuCOh2bX6w3s4F94/tq_18vZ_2Le_LJ6WTjWQgxakhVA"',
+        id: {
+          kind: "youtube#video",
+          videoId: "HcROIlwiuGQ"
+        },
+        snippet: {
+          publishedAt: "2019-12-27T01:00:00.000Z",
+          channelId: "UCVCYPSYZ8jjYz_V5OOOi3rQ",
+          title:
+            "😍 Cute is Not Enough - Cute Kittens In The World #3| CuteVN Animals",
+          description:
+            "More amazing animal moments: https://youtube.com/playlist?list=PLWSopI47S5QOekTUcwVwYFFnFZ83Tnu1_ Subscribe to CuteVN animals for more ...",
+          thumbnails: {
+            default: {
+              url: "https://i.ytimg.com/vi/HcROIlwiuGQ/default.jpg",
+              width: 120,
+              height: 90
+            },
+            medium: {
+              url: "https://i.ytimg.com/vi/HcROIlwiuGQ/mqdefault.jpg",
+              width: 320,
+              height: 180
+            },
+            high: {
+              url: "https://i.ytimg.com/vi/HcROIlwiuGQ/hqdefault.jpg",
+              width: 480,
+              height: 360
+            }
+          },
+          channelTitle: "CuteVN Animals",
+          liveBroadcastContent: "none"
+        }
+      }
+    ]
+  };
+}
